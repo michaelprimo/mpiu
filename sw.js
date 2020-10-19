c769cc02034857e18dae6fc5b88dea63a364fb72
michaelprimo <magiclegendstaff@gmail.com>
2020-10-19 12:18:40 +0200

    sw

--- sw.js
+++ sw.js
@@ -1,20 +1,51 @@
-const version = '1';
+/*
+ *
+ *  Air Horner
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches
-      .open(`static-${version}`)
-      .then((cache) =>
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/index.css',
-          '/sw.js',
-          '/logo.png',
-          '/particles.min.js',
-          '/wallpaper.jpg',
-          '/app.js',
-        ]),
-      ),
+const version = "1";
+const cacheName = `Mpiùagency-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/index.css`,
+        `/app.js`,
+        `/particles-min.js`,
+        `/pwacompat-min.js`,
+        `/snow.mp3`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
