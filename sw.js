6b2bf05bcb429a6e4694e4477c88f3691b9aaa2a
michaelprimo <magiclegendstaff@gmail.com>
2020-11-11 18:38:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  *
  */
 
-const version = "1.0.1";
+const version = "1.0.2";
 const cacheName = `Mpiuagency-${version}`;
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -30,7 +30,7 @@ self.addEventListener('install', e => {
         '/images/ombrello.png',
         '/logo.png',
         `/scripts/pwacompat.min.js`,
-        '/traliccio.png',
+        '/traliccio.svg',
         '/particles.min.js',
         '/particles.json',
         '/app.js'
